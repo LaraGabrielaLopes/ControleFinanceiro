6a52ee22a27f6ceb38a68d4cd61df14f1247dff7
Lara Gabriela <lara_g_lopes@hotmail.com>
2023-06-01 12:39:12 -0300

    alterações após plantão: tabela após carregamento/cálculo total

--- script.js
+++ script.js
@@ -37,8 +37,15 @@ function desenhaTabela() {
     linhasExistentes.forEach((element) => {
         element.remove()
     })
-    
+
+    document.querySelector('tfoot').innerHTML = '';
+
+    var total = 0
+
     for (merc in listaMercadoria) {
+        valorNum = parseFloat(listaMercadoria[merc].valor.replace('R$', '').replace('.', '').replace(',','.'))
+        total += listaMercadoria[merc].tipoTransacao == '-' ? -1 * valorNum : valorNum
+
         document.querySelector('table.tabela tbody').innerHTML +=
         `<tr class="conteudo-dinamico">
             <td>${listaMercadoria[merc].tipoTransacao}</td>
@@ -48,28 +55,24 @@ function desenhaTabela() {
         `
     }
 
-}
-   
-    /*document.querySelector('table.tabela tfoot').innerHTML += `
+    document.querySelector('table.tabela tfoot').innerHTML += `
     <tr>
         <td>Total</td>
         <td></td>
-        <td style="text-align:right; display: block;" id="valor-total"><strong>${totalFormatado}</strong>
+        <td style="text-align:right; display: block;" id="valor-total"><strong>${total.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+            minimumFractionDigits: 2,
+        })}</strong>
         <p>${total > 0 ? "[LUCRO]" : total < 0 ? "[PREJUÍZO]" : ""}</p>
         </td>
-    </tr>`;*/
-     
+    </tr>`
 
+}   
 
+//validação do formulário:
 function cadastrarTransacao(e) {
     e.preventDefault()
-
-    var listaMercadoriaRaw = localStorage.getItem('listaMercadoria')
-    if (listaMercadoriaRaw != null) {
-        var listaMercadoria = JSON.parse(listaMercadoriaRaw)
-    } else {
-        var listaMercadoria = [];
-    }
     
     listaMercadoria.push({
         tipoTransacao: e.target.elements['campos-input'].value,
@@ -79,6 +82,7 @@ function cadastrarTransacao(e) {
 
     localStorage.setItem('listaMercadoria', JSON.stringify(listaMercadoria));
 
+    //limpar campos input:
     function limparCamposInput() {
         document.getElementById('campos-input').value = "-";
         document.getElementById('campo-mercadoria').value = "";
@@ -90,26 +94,32 @@ function cadastrarTransacao(e) {
 
   }
 
-  
-
- //A ÚLTIMA TRANSAÇAO SÓ APARECE DEPOIS DE ATUALIZAR A PÁGINA ???????????????????????????????????????????????????????
-
-
-
 //Limpar tabela:
 function limparTabela() {
-    document.querySelector('table.tabela tbody').innerHTML = "";
-    document.querySelector('table.tabela tfoot').innerHTML = "";
+    document.querySelector('table.tabela tbody').innerHTML = '';
+    document.querySelector('table.tabela tfoot').innerHTML = '';
 }
 
 function limparDados(merc) {
     if (confirm("Tem certeza que deseja limpar os dados da pagina?") == true) {
-      localStorage.clear();
-      listaMercadoria = [];
-      limparTabela();
-      desenhaTabela();
+        localStorage.clear();
+        listaMercadoria = [];
+        limparTabela();
+        desenhaTabela();
+    }
+
+    //desenhando tabela quando não houver transação cadastrada:
+    if (localStorage.length == 0) {
+        document.querySelector("table.tabela tbody").innerHTML += `
+            <tr>
+            <td colspan="3" style ="text-align: center; padding-bottom: 10px"}>
+                Nenhuma transação cadastrada.
+            </td>
+            <tr>`;
     }
 }
+
+
 
 
 
