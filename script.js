96e1b697ae48cc4c2d52771b36ebcd2f5513b720
Lara Gabriela <lara_g_lopes@hotmail.com>
2023-06-07 14:37:24 -0300

    testando a tabela sem transação

--- script.js
+++ script.js
@@ -42,37 +42,48 @@ function desenhaTabela() {
 
     var total = 0
 
-    for (merc in listaMercadoria) {
-        valorNum = parseFloat(listaMercadoria[merc].valor.replace('R$', '').replace('.', '').replace(',','.'));
-        total += listaMercadoria[merc].tipoTransacao == '-' ? -1 * valorNum : valorNum;
+    //desenhando tabela quando não houver transação cadastrada:
 
-        document.querySelector('table.tabela tbody').innerHTML +=
-        `<tr class="conteudo-dinamico">
-            <td>${listaMercadoria[merc].tipoTransacao}</td>
-            <td>${listaMercadoria[merc].nomeMercadoria}</td>
-            <td class="td3">${listaMercadoria[merc].valor}</td>
-        </tr>
-        `
+    if (listaMercadoria.length == 0) {
+        document.querySelector("table.tabela tbody").innerHTML += `
+            <tr>
+                <td colspan="3" style ="text-align: center; padding-bottom: 10px"}>
+                    Nenhuma transação cadastrada.
+                </td>
+            <tr>`;
+    } else if (listaMercadoria.length != 0) {
+        for (merc in listaMercadoria) {
+            valorNum = parseFloat(listaMercadoria[merc].valor.replace('R$', '').replace('.', '').replace(',','.'));
+            total += listaMercadoria[merc].tipoTransacao == '-' ? -1 * valorNum : valorNum;
+    
+            document.querySelector('table.tabela tbody').innerHTML +=
+            `<tr class="conteudo-dinamico">
+                <td>${listaMercadoria[merc].tipoTransacao}</td>
+                <td>${listaMercadoria[merc].nomeMercadoria}</td>
+                <td class="td3">${listaMercadoria[merc].valor}</td>
+            </tr>
+            `
+        }
+    
+        document.querySelector('table.tabela tfoot').innerHTML += `
+        <tr>
+            
+            <td>Total</td>
+            <td></td>
+            <td class="tdValor" id="valor-total"><strong>${total.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+                minimumFractionDigits: 2,
+            })}</strong>
+            <p>${total > 0 ? "[LUCRO]" : total < 0 ? "[PREJUÍZO]" : ""}</p>
+            </td>
+            
+        </tr>`
     }
-
-    document.querySelector('table.tabela tfoot').innerHTML += `
-    <tr>
-        
-        <td>Total</td>
-        <td></td>
-        <td class="tdValor" id="valor-total"><strong>${total.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-            minimumFractionDigits: 2,
-        })}</strong>
-        <p>${total > 0 ? "[LUCRO]" : total < 0 ? "[PREJUÍZO]" : ""}</p>
-        </td>
-        
-    </tr>`
-
-}   
+};   
 
 //validação do formulário:
+
 function cadastrarTransacao(e) {
     e.preventDefault()
     
@@ -85,6 +96,7 @@ function cadastrarTransacao(e) {
     localStorage.setItem('listaMercadoria', JSON.stringify(listaMercadoria));
 
     //limpar campos input:
+
     function limparCamposInput() {
         document.getElementById('campos-input').value = "-";
         document.getElementById('campo-mercadoria').value = "";
@@ -94,13 +106,14 @@ function cadastrarTransacao(e) {
     limparCamposInput()
     desenhaTabela()
 
-  }
+  };
 
 //Limpar tabela:
+
 function limparTabela() {
     document.querySelector('table.tabela tbody').innerHTML = '';
     document.querySelector('table.tabela tfoot').innerHTML = '';
-}
+};
 
 function limparDados(merc) {
     if (confirm("Tem certeza que deseja limpar os dados da pagina?") == true) {
@@ -109,51 +122,4 @@ function limparDados(merc) {
         limparTabela();
         desenhaTabela();
     }
-
-    //desenhando tabela quando não houver transação cadastrada:
-    if (localStorage.length == 0) {
-        document.querySelector("table.tabela tbody").innerHTML += `
-            <tr>
-                <td colspan="3" style ="text-align: center; padding-bottom: 10px"}>
-                    Nenhuma transação cadastrada.
-                </td>
-            <tr>`;
-        document.querySelector("table.tabela tbody").innerHTML += `
-            <tr>
-
-            <tr>`;
-        
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
